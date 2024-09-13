82baf29497bd549ba7fc5e4918ea5b6b8c03a4ea
Mohd Anas <anasmohd4697@gmail.com>
2024-09-14 03:54:19 +0530

    education 13

--- src/Transition.jsx
+++ src/Transition.jsx
@@ -4,7 +4,7 @@ import { motion } from 'framer-motion';
 const Transition = () => {
   return (
     <>
-      <motion.div className='fixed top-0 bottom-0 right-full w-screen z-30 bg-blue-900 dark:bg-primaryDark' 
+      <motion.div className='fixed  top-0 bottom-0 right-full w-screen z-30 bg-[#dc1d22] dark:bg-primaryDark' 
       initial={{x:'100%', width:'100%'}}
       animate={{x:'0%', width:'0%'}}
       exit={{x:['0%','100%'],width:['0%','100%']}}
@@ -15,7 +15,7 @@ const Transition = () => {
       animate={{x:'0%', width:'0%'}}
       transition={{delay:0.2, duration:0.8, ease:'easeInOut'}}
       />
-      <motion.div className='fixed top-0 bottom-0 right-full w-screen z-10 bg-[#e89835] dark:bg-light' 
+      <motion.div className='fixed top-0 bottom-0 right-full w-screen z-10 bg-[#1e313f] dark:bg-light' 
       initial={{x:'100%', width:'100%'}}
       animate={{x:'0%', width:'0%'}}
       transition={{delay:0.4, duration:0.8, ease:'easeInOut'}}
